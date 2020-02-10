a2f6b23a48dde3c8d2542059cd2b2fb24deb9e4d
Y1a2s3h4 <yashpurohit763@gmail.com>
2020-02-10 21:27:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import logo from "./logo.svg";
+import Particles from "react-particles-js";
+import "./App.css";
+
+const particlesOpt = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 850
+      }
+    },
+    move: {
+      speed: 12
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true
+      }
+    }
+  }
+};
+function App() {
+  return (
+    <div className="particle_bg">
+      <Particles params={particlesOpt} />
+      <div className="App-container">
+        <img src={logo} className="App-logo" />
+        <h1 className="App-Title">Learning ReactJS</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
